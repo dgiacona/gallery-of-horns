5c755af0f30d3a82bf7a10258118672a2b0b0c34
Dominic Giacona <dominic@exobyte.io>
2022-06-14 22:37:26 -0400

    lab02 with correct branch

--- README.md
+++ README.md
@@ -68,3 +68,9 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+- lab 02
+- Estimated time needed to complete: 1.5 hours
+- Start Time 8:10
+- End Time: 9:30
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,6 +1,21 @@
 import React from 'react';
-
+import Button from 'react-bootstrap/Button';
+import './HornedBeast.css';
 class HornedBeast extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        hearts: 0
+      };
+    };
+
+handleHearts = () => {
+  this.setState({
+    hearts: this.state.hearts + 1,
+    favorite: 'Photo added to favorites!'
+  })
+}
+
   render () {
     return (
       <article>
@@ -8,6 +23,8 @@ class HornedBeast extends React.Component {
         <img src={this.props.image_url} alt={this.props.description}
         title={this.props.title}/>
         <p>{this.props.description}</p>
+        <p>{this.state.hearts} 💜 </p>
+        <p onClick={this.handleHearts}><Button>Favorite</Button></p>
       </article>
     )
   }
